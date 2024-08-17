b7449f6c26c1de39dae378fd641120ed7d12f56f
Teslule <luletesfaye2020@gmail.com>
2024-08-17 17:07:41 -0400

    backend commit

---
+++ db/dbConfig.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mysql2 = require("mysql2");
+
+const dbConnection = mysql2.createPool({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  socketPath: process.env.DB_SOCKET_PATH,
+  connectionLimit: 10,
+});
+
+dbConnection.execute("select 'test'", (err, result) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(result);
+  }
+});
+
+module.exports = dbConnection.promise();
